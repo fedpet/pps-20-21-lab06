454b685df3d05e58796cb92b314bd660b0bbc625
fedpet <federico.pettinari2@studio.unibo.it>
2021-04-12 11:44:57 +0200

    Ex1

---
+++ test/u06lab/solution/TryFunctions.scala
@@ -0,0 +1,17 @@
+package u06lab.solution
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.{Assertions, Test}
+
+class TryFunctions {
+  @Test
+  def testFunctions() {
+    val f: Functions = FunctionsImpl
+    assertEquals(60.1, f.sum(List(10.0, 20.0, 30.1)), 0.001) // 60.1
+    assertEquals(0.0, f.sum(List())) // 0.0
+    assertEquals("abc", f.concat(Seq("a", "b", "c")))
+    assertEquals("", f.concat(Seq()))
+    assertEquals(3, f.max(List(-10, 3, -5, 0)))
+    assertEquals(Integer.MIN_VALUE, f.max(List()))
+  }
+}
